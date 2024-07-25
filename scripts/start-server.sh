a84366d9baa9991a71d4c85a56f0371f458b2bca
m020202 <m020202@naver.com>
2024-07-25 23:25:58 +0900

    sh 파일 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "-------------서버 시작----------------"
+echo "---제발 잘 되게 해주세요----"
 docker stop instagram-sever || true
 docker rm instagram-sever || true
 docker pull 058264543539.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
 docker run -d --name instagram-server -p 8080:8080 058264543539.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
-echo "-------------서버 배포 끝---------------"
+echo "-------------제발요---------------"
