9032ec69b9ade195bef13ef73edcca190217c01f
m020202 <m020202@naver.com>
2024-07-25 12:52:39 +0900

    edit

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "-------------서버 시작----------------"
-cd /home/ubuntu/instagram-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop instagram-server || true
+docker rm instagram-server || true
+docker pull 058264543539.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
+docker run -d --name instagram-sever -p 8080:8080 058264543539.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
 echo "-------------서버 배포 끝---------------"
